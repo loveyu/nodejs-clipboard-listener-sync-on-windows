f708ec2a1815347b00b2b590b966a46f81ffd146
loveyu <i@loveyu.info>
2017-05-16 00:23:20 +0800

    处理事件异常及循环调用的问题

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ let clipboard_event = new ClipboardEvent();
 
 clipboard_event.addListener(function (data, type) {
     console.log("ClipboardEvent.CF_TEXT", data);
-}, ClipboardEvent.CF_TEXT);
+}, clipboard_event.CF_TEXT);
 
 clipboard_event.addListener(function (data, type) {
     console.log("ClipboardEvent.CF_BITMAP", data);
-}, ClipboardEvent.CF_BITMAP);
+}, clipboard_event.CF_BITMAP);
